ce7f98d8f0cd59f7ec12f42be1da92e11ebba235
Steven Olson <steveoscaro@gmail.com>
2015-09-17 22:12:27 -0600

    Quiz 1.1 for tealeaf

---
+++ quiz_1_1.rb
@@ -0,0 +1,7 @@
+#The code below will print out the array elements on new lines, as the 'puts' command 
+#includes a line break. The .uniq method did not mutate the caller. 
+
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
