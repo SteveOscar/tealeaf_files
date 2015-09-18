d6fd6a40a36b6a171820de43873b5c2ff240f98b
Steven Olson <steveoscaro@gmail.com>
2015-09-17 22:21:51 -0600

    Quiz 1.2 for tealeaf

---
+++ quiz_1_2.rb
@@ -0,0 +1,6 @@
+# Seeing a '!' on the end of a method often means that the method mutates the caller. 
+# A '?' can mean that the method returns a boolean value. But these symbols when used 
+# as part of method names are not syntax.
+
+#    != means does not equal, and I have used it in conditional statements for comaprisons
+#    !! returns the boolean value of the obect prepended (! gives the opposite value)
