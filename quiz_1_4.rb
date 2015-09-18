20942cae3bf13ad8d4482256fc355c9e27589402
Steven Olson <steveoscaro@gmail.com>
2015-09-17 22:37:12 -0600

    Quiz 1.4 for tealeaf

---
+++ quiz_1_4.rb
@@ -0,0 +1,10 @@
+# the delete_at method refers to the item in the index value, so it would delete the '2' from the original array and return 2
+# the delelte method refers to a specific value in the array
+
+numbers = [1, 2, 3, 5, 7]
+#What would the following return?
+
+p numbers.delete_at(1)
+p numbers.delete(5)
+
+p numbers
