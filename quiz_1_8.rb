269f424f99f04420c6b9f88054d77641dec981ed
Steven Olson <steveoscaro@gmail.com>
2015-09-17 22:51:49 -0600

    Quiz 1.8 for tealeaf

---
+++ quiz_1_8.rb
@@ -0,0 +1,9 @@
+flintstones = ["Fred", "Wilma"]
+flintstones << ["Barney", "Betty"]
+flintstones << ["BamBam", "Pebbles"]
+
+flintstones.flatten!
+
+p flintstones
+
+# the flatten! method unnests the array
