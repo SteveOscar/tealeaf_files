31e1a9e1741f19dd1a60217bba5c5697987631f5
Steven Olson <steveoscaro@gmail.com>
2015-10-03 15:03:56 -0600

    Lesson 2 Rock Paper Scissors

--- lesson_2_rps.rb
+++ lesson_2_rps.rb
@@ -1,17 +1,20 @@
+# Rock Paper Scissors
 CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   puts ">> #{message}"
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt 'You win!'
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt 'You lose...'
   else
     prompt 'Tie game...'
